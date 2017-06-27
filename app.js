dd87bcb4adad89cb36d360707e3029aa3665677e
Emilianosanchezlopez <emilianosanchezlopez@hotmail.com>
2017-06-27 13:01:43 -0500

    to do list app
    files for to do list app

--- app.js
+++ app.js
@@ -1,52 +1,77 @@
 var azure = require('azure-storage');
+var nconf = require('nconf');
+nconf.env()
+.file({ file: 'config.json', search: true });
+var tableName = nconf.get("TABLE_NAME");
+var partitionKey = nconf.get("PARTITION_KEY");
+var accountName = nconf.get("STORAGE_NAME");
+var accountKey = nconf.get("STORAGE_KEY");
 
-accountName = "tabla01";
-accountKey = "G1ohxJ0+kY1jAdQmXIisulO2Bsk8ao3btRx8RbnvmJLyZZCqmIDgN+UvenAID6cCtarqXh/7Myno/jU5dw1yeA=="
 
-var tableService = azure.createTableService(accountName, accountKey);
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-tableService.createTableIfNotExists('mytableMarte', function(error, result, response) {
-  if (!error) {
-    console.log(response);
-  }
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', routes);
+// app.use('/users', users);
+var TaskList = require('./routes/tasklist');
+var Task = require('./models/task');
+var task = new Task(azure.createTableService(accountName, accountKey), tableName, partitionKey);
+var taskList = new TaskList(task);
+
+app.get('/', taskList.showTasks.bind(taskList));
+app.post('/addtask', taskList.addTask.bind(taskList));
+app.post('/completetask', taskList.completeTask.bind(taskList));
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-var entGen = azure.TableUtilities.entityGenerator;
+/// error handlers
 
-var entity = {
-  PartitionKey: entGen.String('part2'),
-  RowKey: entGen.String('row1'),
-  boolValueTrue: entGen.Boolean(true),
-  boolValueFalse: entGen.Boolean(false),
-  intValue: entGen.Int32(42),
-  dateValue: entGen.DateTime(new Date(Date.UTC(2011, 10, 25))),
-  complexDateValue: entGen.DateTime(new Date(Date.UTC(2013, 02, 16, 01, 46, 20)))
-};
-var task3 = {
-  PartitionKey: {'_':'hometasks'},
-  RowKey: {'_': '2'},
-  description: {'_':'Renglon2222222222222sssss'},
-  dueDate: {'_':new Date(2015, 6, 20), '$':'Edm.DateTime'}
-};
-var task2 = {
-  PartitionKey: {'_':'hometasks2'},
-  RowKey: {'_': '2'},
-  description: {'_':'take out the trash!!!!!!!!!!!!'},
-  dueDate: {'_':new Date(2015, 7, 20), '$':'Edm.DateTime'}
-};
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
 
-tableService.insertEntity('mytableMarte', task3, function(error, result, response) {
-  if (!error) {
-    console.log(result);
-  }
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
-tableService.retrieveEntity('mytableMarte', 'hometasks', '2', function(error, result, response) {
-  if (!error) {
-    console.log(result);
-  }
-});
-
-//console.log(tableService.retrieveEntity);
-
+module.exports = app;
